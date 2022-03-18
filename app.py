a57bfa44666359cc94633a64647d3f49d7a3bd66
David Sampimon <davidsampimon@gmail.com>
2022-03-18 15:29:47 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+# import modelks
+
+# main menu - add, search, analysis, exit, view
+
+# add books
+
+# edit books
+
+# delete books
+
+# search books
+
+# data cleaning
+
+# loop runs program
---
+++ models.py
@@ -0,0 +1,7 @@
+# create a db
+
+# give it a name
+
+# create a model
+
+# title, author, date published, price
